cbb8cb7017db710b823bfb3a0e9247c94049db6c
Josemy Duarte <duartejosemy@gmail.com>
2016-12-01 13:27:42 -0400

    Add html response

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"dolartodaydeamon/controller"
 	"dolartodaydeamon/model"
 	"flag"
+	"github.com/kataras/iris"
+	"github.com/mitchellh/hashstructure"
+	"github.com/square/go-jose/json"
 	"github.com/vharitonsky/iniflags"
 	"gopkg.in/mgo.v2"
 	"log"
 	"time"
-	"github.com/mitchellh/hashstructure"
 )
 
 var (
@@ -19,6 +21,7 @@ var (
 	dbPassword       = flag.String("dbPassword", "", "Password del usuario de la base de datos")
 	dbSource         = flag.String("dbSource", "", "Base de datos utilizada para establecer credenciales y privilegios")
 	dbColIndicadores = flag.String("dbColIndicadores", "indicadores", "Colección para los indicadores")
+	minCheckDT       = flag.Int("minCheckDT", 1, "Minutos para revisar DolarToday por cambios")
 )
 
 func main() {
@@ -42,50 +45,70 @@ func main() {
 
 	idg := controller.NewIDGenerator(db)
 
-	for 1 == 1 {
-		dolarToday := model.Indicadores{}
-		controller.GetJson(*dolarTodayUrl, &dolarToday.DOLARTODAY)
+	go func() {
+		for 1 == 1 {
+			dolarToday := model.Indicadores{}
+			controller.GetJson(*dolarTodayUrl, &dolarToday.DOLARTODAY)
 
+			var results model.Indicadores
+			err = colIndicadores.Find(nil).Sort("-metadata.n").One(&results)
+			if err == mgo.ErrNotFound || err == nil {
+
+				log.Printf("BD Secuencia: %d", results.METADATA.Secuencia)
+				log.Printf("BD: %+v", results.DOLARTODAY)
+
+				hashBD, err := hashstructure.Hash(results.DOLARTODAY, nil)
+				if err != nil {
+					panic(err)
+				}
+
+				log.Printf("Hash BD: %d", hashBD)
+				log.Printf("DT: %+v", dolarToday.DOLARTODAY)
+
+				hashDT, err := hashstructure.Hash(dolarToday.DOLARTODAY, nil)
+				if err != nil {
+					panic(err)
+				}
+
+				log.Printf("Hash DT: %d", hashDT)
+
+				if hashDT != hashBD {
+					log.Printf("HASH DT != BD: %d != %d", hashDT, hashBD)
+					n, err := idg.Next("indicadores")
+					if err != nil {
+						panic(err)
+					}
+					withMetadata := model.Metadata{}
+					withMetadata.Metadata(n)
+					dolarToday.AgregarMetadata(withMetadata)
+					err = colIndicadores.Insert(&dolarToday)
+					if err != nil {
+						panic(err)
+					}
+
+				}
+			} else {
+				panic(err)
+			}
+			time.Sleep(time.Duration(*minCheckDT) * time.Minute)
+		}
+	}()
+
+	iris.Get("/", func(c *iris.Context) {
 		var results model.Indicadores
-		err = colIndicadores.Find(nil).Sort("-metadata.n").One(&results)
+		err := colIndicadores.Find(nil).Sort("-metadata.n").One(&results)
 		if err == mgo.ErrNotFound || err == nil {
-
-			log.Printf("BD Secuencia: %d", results.METADATA.Secuencia)
-			log.Printf("BD: %+v", results.DOLARTODAY)
-
-			hashBD, err := hashstructure.Hash(results.DOLARTODAY, nil)
+			out, err := json.MarshalIndent(results, "", "  ")
 			if err != nil {
-				panic(err)
-			}
-
-			log.Printf("Hash BD: %d", hashBD)
-			log.Printf("DT: %+v", dolarToday.DOLARTODAY)
-
-			hashDT, err := hashstructure.Hash(dolarToday.DOLARTODAY, nil)
-			if err != nil {
-				panic(err)
-			}
-
-			log.Printf("Hash DT: %d", hashDT)
-
-			if hashDT != hashBD {
-				log.Printf("HASH DT != BD: %d != %d", hashDT, hashBD)
-				n, err := idg.Next("indicadores")
-				if err != nil {
-					panic(err)
-				}
-				withMetadata := model.Metadata{}
-				withMetadata.Metadata(n)
-				dolarToday.AgregarMetadata(withMetadata)
-				err = colIndicadores.Insert(&dolarToday)
-				if err != nil {
-					panic(err)
-				}
+				c.HTML(iris.StatusInternalServerError, err.Error())
+			} else {
+				c.HTML(iris.StatusOK, string(out))
 
 			}
 		} else {
-			panic(err)
+			c.HTML(iris.StatusInternalServerError, err.Error())
 		}
-		time.Sleep(60 * 15 * time.Second)
-	}
+	})
+
+	iris.Listen(":8080")
 }
